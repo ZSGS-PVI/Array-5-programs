5667afc901831c685d93fda4f84f3da5f90d6bb7
kalpana@77 <102209171+kalpanadev22@users.noreply.github.com>
2023-10-24 17:22:20 +0530

    Update WeightBsedSort.java

--- WeightBsedSort.java
+++ WeightBsedSort.java
@@ -6,64 +6,61 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class WeightBsedSort {
-	 public static int calculateWeight(int num) {
-	        int weight = 0;
+    public static int calculateWeight(int num) {
+        int weight = 0;
 
-	        if (isPerfectSquare(num)) {
-	            weight += 5;
-	        }
-	        if (num % 4 == 0 && num % 6 == 0) {
-	            weight += 4;
-	        }
-	        if (num % 2 == 0) {
-	            weight += 3;
-	        }
+        if (isPerfectSquare(num)) {
+            weight += 5;
+        }
+        if (num % 4 == 0 && num % 6 == 0) {
+            weight += 4;
+        }
+        if (num % 2 == 0) {
+            weight += 3;
+        }
 
-	        return weight;
-	    }
+        return weight;
+    }
 
-	    public static boolean isPerfectSquare(int num) {
-	        int sqrt = (int) Math.sqrt(num);
-	        return sqrt * sqrt == num;
-	    }
+    public static boolean isPerfectSquare(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        return sqrt * sqrt == num;
+    }
 
-	public static void main(String[] args) {
-		
-		Scanner in=new Scanner(System.in);
-      System.out.println("Enter the length of the array you want:");
-      int len=in.nextInt();
-      
-      System.out.println("Enter the values:");
-      int[] array=new int[len];
-      for(int i=0;i<len;i++) {
-    	  array[i]=in.nextInt();
-      }
-      ArrayList<Pair> weightedNumbers = new ArrayList<>();
-      for (int num : array) {
-          int weight = calculateWeight(num);
-          weightedNumbers.add(new Pair(num, weight));
-      }
-      
-      
-      Collections.sort(weightedNumbers, new Comparator<Pair>() {
-          public int compare(Pair a, Pair b) {
-              return b.weight - a.weight;
-          }
-      });
-      
-      for (Pair pair : weightedNumbers) {
-          System.out.print("<" + pair.number + ", " + pair.weight + "> ");
-      }
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of the array you want:");
+        int len = in.nextInt();
 
-}
+        System.out.println("Enter the values:");
+        int[] array = new int[len];
+        int[] weights = new int[len];
 
-class Pair {
-    int number;
-    int weight;
+        for (int i = 0; i < len; i++) {
+            array[i] = in.nextInt();
+            weights[i] = calculateWeight(array[i]);
+        }
 
-    public Pair(int number, int weight) {
-        this.number = number;
-        this.weight = weight;
+        // Sort the array based on weights in descending order.
+	    
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (weights[i] < weights[j]) {
+                    int temp = weights[i];
+                    weights[i] = weights[j];
+                    weights[j] = temp;
+
+			
+  // Swap corresponding values in the array as well.
+                    int tempValue = array[i];
+                    array[i] = array[j];
+                    array[j] = tempValue;
+                }
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            System.out.print("<" + array[i] + ", " + weights[i] + "> ");
+        }
     }
 }
